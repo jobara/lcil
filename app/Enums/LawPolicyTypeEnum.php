696148a263833ecd3f59e9b5828a01fe9404621e
Justin Obara <obara.justin@gmail.com>
2022-04-25 16:15:50 -0400

    style: alphabetize enums

--- app/Enums/LawPolicyTypeEnum.php
+++ app/Enums/LawPolicyTypeEnum.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum LawPolicyTypeEnum: string
 {
-    case POLICY = 'policy';
-    case CONSTITUTIONAL = 'constitutional';
     case CASE_LAW = 'case law';
-    case REGULATION = 'regulation';
+    case CONSTITUTIONAL = 'constitutional';
+    case POLICY = 'policy';
     case QUASI_CONSTITUTIONAL = 'quasi-constitutional';
+    case REGULATION = 'regulation';
 }
--- app/Enums/LegalChallengeTypeEnum.php
+++ app/Enums/LegalChallengeTypeEnum.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum LegalChallengeTypeEnum: string
 {
-    case PERSONAL = 'personal life and care';
-    case HEALTH = 'health care';
     case FINANCIAL = 'financial and property';
+    case HEALTH = 'health care';
+    case PERSONAL = 'personal life and care';
 }

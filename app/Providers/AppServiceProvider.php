9df650b8dac9ed319e23e054ac3d305498a8544f
Justin Obara <obara.justin@gmail.com>
2022-09-18 09:34:11 -0400

    feat!: force HTTPS when not in local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,8 +23,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        if (config('app.env') !== 'local') {
+            $url->forceScheme('https');
+        }
+
         Paginator::defaultView('components.pagination-links');
 
         Paginator::defaultSimpleView('components.simple-pagination-links');

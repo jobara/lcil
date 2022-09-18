860b5dac353ee59a4b9b6a3f2eedc4c3095a5b6c
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2022-09-18 17:52:08 -0300

    feat: add maintenance view

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,9 @@
+<x-guest-layout>
+    <x-slot name="title">{{ __('Site unavailable') }}</x-slot>
+
+    <h1>{{ __('Site unavailable') }}</h1>
+    <p>
+        {{ __('The site is currently undergoing maintenance.') }}<br />
+        {{ __('Please wait a few moments and try again.') }}
+    </p>
+</x-guest-layout>
